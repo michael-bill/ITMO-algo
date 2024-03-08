9a7fb16519f145277c19cb6a63425f725ccf47eb
Michael-Bill <biloshitskiy_m@mail.ru>
2024-03-08 20:24:16 +0300

    add: block1 - task2, passed...

---
+++ block1/task2/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <stack>
+
+int32_t to_lower(int32_t s);
+int32_t to_upper(int32_t s);
+bool is_lower(int32_t s);
+bool is_upper(int32_t s);
+
+// Либо животное, либо ловушка
+// type - код символа
+// number - порядковый номер в строке
+struct zoo_object {
+    int32_t type;
+    size_t number;
+};
+
+int main(int argc, char** argv) {
+    using namespace std;
+    string line;
+    cin >> line;
+    // Стек для ловушек и животных
+    stack<zoo_object> zoo_objects;
+    // Счётчики животных и ловушек
+    size_t animals_counter = -1, traps_counter = -1;
+    // Массив-результат
+    size_t* trapped_animals = new size_t[line.size() / 2];
+    for (size_t i  = 0; i < line.size(); i++) {
+        int32_t s = line[i];
+        // В случае, если животное
+        if (is_lower(s)) {
+            animals_counter++;
+            int32_t trap = to_upper(s);
+            if (!zoo_objects.empty() && zoo_objects.top().type == trap) {
+                trapped_animals[zoo_objects.top().number] = animals_counter;
+                zoo_objects.pop();
+            } else {
+                zoo_object a = { .type = s, .number = animals_counter };
+                zoo_objects.push(a);
+            }
+        // В случае, если ловушка
+        } else if (is_upper(s)) {
+            traps_counter++;
+            int32_t animal = to_lower(s);
+            if (!zoo_objects.empty() && zoo_objects.top().type == animal) {
+                trapped_animals[traps_counter] = zoo_objects.top().number;
+                zoo_objects.pop();
+            } else {
+                zoo_object t = { .type = s, .number = traps_counter };
+                zoo_objects.push(t);
+            }
+        }
+    }
+    // Все попались
+    if (zoo_objects.empty()) {
+        cout << "Possible" << endl;
+        size_t n = line.size() / 2;
+        for (size_t i = 0; i < n; i++) {
+            cout << trapped_animals[i] + 1;
+            if (i != n - 1) cout << ' ';
+            else cout << endl; 
+        }
+    } else {
+        cout << "Impossible" << endl;
+    }
+    delete[] trapped_animals;
+    return 0;
+}
+
+int32_t to_lower(int32_t s) {
+    if (s >= 'A' && s <= 'Z') {
+        return s - 'A' + 'a';
+    }
+    return s;
+}
+
+int32_t to_upper(int32_t s) {
+    if (s >= 'a' && s <= 'z') {
+        return s - 'a' + 'A';
+    }
+    return s;
+}
+
+bool is_lower(int32_t s) {
+    if (s >= 'a' && s <= 'z') return true;
+    return false;
+}
+
+bool is_upper(int32_t s) {
+    if (s >= 'A' && s <= 'Z') return true;
+    return false;
+}
