d96b51e67b406f4933f88baebf8f28e8425a1ba0
Michael B. <biloshitskiy_m@mail.ru>
2024-04-21 01:53:57 +0300

    add: block3 - task1, passed

--- block3/task1/main.cpp
+++ block3/task1/main.cpp
@@ -2,32 +2,42 @@
 #include <vector>
 #include <set>
 
-using namespace std;
+#define NOT_SEEN -1
 
 int main() {
-  int n, k, p, car;
-  cin >> n >> k >> p;
+    using namespace std;
+    int32_t n, k, p, car;
+    cin >> n >> k >> p;
 
-  vector<int> last_seen(n, -1), next_use(p, p);
-  for (int i = 0; i < p; ++i) {
-    cin >> car;
-    --car;
-    if (last_seen[car] != -1) next_use[last_seen[car]] = i;
-    last_seen[car] = i;
-  }
+    // last_seen[i] хранит индекс последнего появления машинки i+1
+    // next_use[i] хранит индекс следующего использования машинки i+1
+    vector<int32_t> last_seen(n, NOT_SEEN), next_use(p, p);
 
-  int count = 0;
-  set<int> on_floor;
-  for (int i = 0; i < p; ++i) {
-    if (!on_floor.count(i)) {
-      ++count;
-      if (on_floor.size() == k) on_floor.erase(prev(on_floor.end()));
-    } else {
-      on_floor.erase(i);
+    // Считываем последовательность машинок и заполняем next_use
+    for (int32_t i = 0; i < p; i++) {
+        cin >> car;
+        car--;
+        if (last_seen[car] != NOT_SEEN)
+            next_use[last_seen[car]] = i;
+        last_seen[car] = i;
     }
-    on_floor.insert(next_use[i]);
-  }
 
-  cout << count << endl;
-  return 0;
+    int32_t res = 0;
+    // on_floor - индексы машинок, которые сейчас на полу
+    set<int32_t> on_floor;
+    for (int32_t i = 0; i < p; i++) {
+        if (on_floor.count(i) <= 0) {
+            res++;
+            if (on_floor.size() == k) {
+                auto it = on_floor.end();
+                on_floor.erase(prev(it));
+            }
+            on_floor.insert(next_use[i]);
+            continue;
+        }
+        on_floor.erase(i);
+        on_floor.insert(next_use[i]);
+    }
+    cout << res << endl;
+    return 0;
 }
