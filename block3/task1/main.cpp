c9feb9ec6089abece674b3b1e68981e367159ccb
Michael B. <biloshitskiy_m@mail.ru>
2024-04-21 01:32:49 +0300

    add: block3 - task3, not passed 2

--- block3/task1/main.cpp
+++ block3/task1/main.cpp
@@ -3,30 +3,38 @@
 #include <unordered_set>
 
 int main() {
-    int32_t num_cars, max_floor_cars, num_plays;
-    std::cin >> num_cars >> max_floor_cars >> num_plays;
+    using namespace std;
+    int32_t n, k, p;
+    cin >> n >> k >> p;
 
-    std::queue<int32_t> sequence;
-    for (int32_t i = 0; i < num_plays; i++) {
-        int32_t car_id;
-        std::cin >> car_id;
-        sequence.push(car_id);
+    // Результат - минимальное количество операций
+    int32_t res = 0;
+    // Очередь запросов на машинки от Пети
+    queue<int32_t> requests;
+    // Множество машинок на полу
+    unordered_set<int32_t> on_floor;
+    int32_t car;
+    for (int32_t i = 0; i < p; i++) {
+        cin >> car;
+        requests.push(car);
     }
-
-    int32_t operations = 0;
-    std::unordered_set<int32_t> cars_on_floor;
-
-    while (!sequence.empty()) {
-        int32_t current_car = sequence.front();
-        sequence.pop();
-        if (cars_on_floor.count(current_car) == 0) {
-            operations++;
-            if (cars_on_floor.size() == max_floor_cars) {
-                cars_on_floor.erase(cars_on_floor.begin());
+    while (!requests.empty()) {
+        car = requests.front();
+        // Если машинка уже не лежит на полу
+        if (on_floor.count(car) == 0) {
+            if (on_floor.size() < k) {
+                // Если на полу меньше K машинок, просто ставим новую
+                on_floor.insert(car);
+                res++;
+            } else {
+                // Иначе, убираем одну из имеющихся машинок и ставим новую
+                on_floor.erase(on_floor.begin());
+                on_floor.insert(car);
+                res++;
             }
-            cars_on_floor.insert(current_car);
         }
+        requests.pop();
     }
-    std::cout << operations << std::endl;
+    cout << res << endl;
     return 0;
 }
