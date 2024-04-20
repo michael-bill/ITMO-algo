39dff21a777fda09387cfa1d0ff1861f3c611414
Michael B. <biloshitskiy_m@mail.ru>
2024-04-20 22:07:23 +0300

    add: block3 - task3, not passed 5

--- block3/task3/main.cpp
+++ block3/task3/main.cpp
@@ -32,8 +32,8 @@ int main(int argc, char** argv) {
     // Заполняем свободное пространство
     if (query != n) {
         if (query > n) query = 0;
-        size_block.insert({n - query, { query + 1, n } });
-        start_block[query + 1] = { query + 1, n };
+        size_block.insert({n - query, { query + ((int64_t)1), n } });
+        start_block[query + ((int64_t)1)] = { query + ((int64_t)1), n };
     }
 
     for (int64_t i = 2; i <= m; i++) {
@@ -43,7 +43,7 @@ int main(int argc, char** argv) {
             int64_t block_size = query;
             // Поиск блока из освобождённых, в который можно влезть
             auto it = size_block.lower_bound(block_size);
-            if (it != size_block.end() && block_size <= it->first) {
+            if (block_size <= it->first) {
                 // Найден блок, который подходит по размеру
                 block b = it->second;
                 // Удаляем его из освобождённых блоков
@@ -51,14 +51,14 @@ int main(int argc, char** argv) {
                 // Удаляем его также из блоков, отсортированных по началу
                 start_block.erase(b.start);
                 // Разбиваем блок на две части, если блок больше запрашиваемого размера
-                if (b.end - b.start + 1 > block_size) {
+                if (b.end - b.start + ((int64_t)1) > block_size) {
                     // Добавляем оставшуюся часть блока в освобождённые блоки
-                    size_block.insert({ b.end - b.start + 1 - block_size, { b.start + block_size, b.end } });
+                    size_block.insert({ b.end - b.start + ((int64_t)1) - block_size, { b.start + block_size, b.end } });
                     // Добавляем оставшуюся часть блока в блоки, отсортированные по началу
                     start_block[b.start + block_size] = { b.start + block_size, b.end };
                 }
                 // Выделяем блок
-                number_block[i] = { b.start, b.start + block_size - 1 }; 
+                number_block[i] = { b.start, b.start + block_size - ((int64_t)1) }; 
                 // Выводим номер первой ячейки памяти в выделенном блоке
                 cout << b.start << endl;
             } else {
@@ -69,21 +69,23 @@ int main(int argc, char** argv) {
         // Если запрос на освобождение памяти
         else if (query < 0) {
             int64_t number = -query;
+            if (number_block.find(number) == number_block.end()) continue;
             block b = number_block[number];
+
             // Убираем блок из занятых
             number_block.erase(number);
 
             // Находим левый прилегающий блок
             auto it = start_block.lower_bound(b.start);
-            if (it != start_block.begin() && it != start_block.end()) {
+            if (it != start_block.begin()) {
                 it--;
                 block left_b = it->second;
-                if (left_b.end + 1 == b.start) {
+                if (left_b.end + ((int64_t)1) == b.start) {
                     // Сливаем блоки
                     b = { left_b.start, b.end };
                     // Удаляем левый блок
                     start_block.erase(left_b.start);
-                    size_block.erase(left_b.end - left_b.start + 1);
+                    size_block.erase(left_b.end - left_b.start + ((int64_t)1));
                 }
             }
 
@@ -91,17 +93,17 @@ int main(int argc, char** argv) {
             it = start_block.upper_bound(b.start);
             if (it != start_block.end()) {
                 block right_b = it->second;
-                if (b.end + 1 == right_b.start) {
+                if (b.end + ((int64_t)1) == right_b.start) {
                     // Сливаем блоки
                     b = { b.start, right_b.end };
                     // Удаляем правый блок
                     start_block.erase(right_b.start);
-                    size_block.erase(right_b.end - right_b.start + 1);
+                    size_block.erase(right_b.end - right_b.start + ((int64_t)1));
                 }
             }
 
             // Добавляем в освобождённые, отсортрированные по размеру
-            size_block.insert({ b.end - b.start + 1, b });
+            size_block.insert({ b.end - b.start + ((int64_t)1), b });
             // Добавляем в освобождённые, отсортрированные по началу
             start_block[b.start] = b;
         }
