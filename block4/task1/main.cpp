c40a9b1503315df43ad3979315400d9751b336d8
Michael-Bill <biloshitskiy_m@mail.ru>
2024-05-19 23:11:39 +0300

    add:block4 - task1, not passed ML 18

--- block4/task1/main.cpp
+++ block4/task1/main.cpp
@@ -3,7 +3,7 @@
 #include <queue>
 using namespace std;
 
-#define INF 2147483647
+#define INT32_MAX 2147483647
 
 #define FIELD '.'
 #define FOREST 'W'
@@ -21,21 +21,8 @@ bool in_field(int32_t x, int32_t y, int32_t n, int32_t m) {
     return x >= 0 && x < n && y >= 0 && y < m;
 }
 
-char get_dur(vector<vector<char>>& field, int32_t n, int32_t m, int32_t x, int32_t y) {
-    return in_field(x, y, n, m) ? (field[x][y] == FIELD ? FIELD_DUR : FOREST_DUR) : -1;
-}
-
-void calc_dest(vector<vector<char>>& field, vector<vector<int32_t>>& dist, int32_t n, int32_t m, int32_t prev_x, int32_t prev_y, int32_t x, int32_t y, int32_t time, vector<vector<vector<char>>>& dirs, char dir) {
-    if (!in_field(x, y, n, m)) return;
-    if (field[x][y] != WATER && dist[x][y] > time) {
-        dist[x][y] = time;
-        dirs[x][y] = dirs[prev_x][prev_y];
-        if (dir != '\0') dirs[x][y].push_back(dir);
-        calc_dest(field, dist, n, m, x, y, x + 1, y, time + get_dur(field, n, m, x, y), dirs, DOWN);
-        calc_dest(field, dist, n, m, x, y, x, y + 1, time + get_dur(field, n, m, x, y), dirs, RIGHT);
-        calc_dest(field, dist, n, m, x, y, x, y - 1, time + get_dur(field, n, m, x, y), dirs, LEFT);
-        calc_dest(field, dist, n, m, x, y, x - 1, y, time + get_dur(field, n, m, x, y), dirs, UP);
-    }
+int get_dur(char cell) {
+    return cell == FIELD ? FIELD_DUR : FOREST_DUR;
 }
 
 int main(int argc, char** argv) {
@@ -50,21 +37,46 @@ int main(int argc, char** argv) {
     dest_y--;
 
     vector<vector<char>> field(n, vector<char>(m));
-    vector<vector<int32_t>> dist(n, vector<int32_t>(m));
+    vector<vector<int32_t>> dist(n, vector<int32_t>(m, INT32_MAX));
+    vector<vector<string>> directions(n, vector<string>(m, ""));
 
     for (int32_t i = 0; i < n; i++) {
         for (int32_t j = 0; j < m; j++) {
             cin >> field[i][j];
-            dist[i][j] = INF;
         }
     }
 
-    vector<vector<vector<char>>> directions(n, vector<vector<char>>(m));
-    calc_dest(field, dist, n, m, start_x, start_y, start_x, start_y, 0, directions, '\0');
-    
+    queue<pair<int32_t, int32_t>> q;
+    q.push({start_x, start_y});
+    dist[start_x][start_y] = 0;
+
+    int dx[] = {-1, 1, 0, 0};
+    int dy[] = {0, 0, -1, 1};
+    char dir[] = {UP, DOWN, LEFT, RIGHT};
+
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; ++i) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (in_field(nx, ny, n, m) && field[nx][ny] != WATER) {
+                int new_dist = dist[x][y] + get_dur(field[nx][ny]);
+                if (new_dist < dist[nx][ny]) {
+                    dist[nx][ny] = new_dist;
+                    directions[nx][ny] = directions[x][y] + dir[i];
+                    q.push({nx, ny});
+                }
+            }
+        }
+    }
+
     int32_t time = dist[dest_x][dest_y];
-    vector<char> path = directions[dest_x][dest_y];
-    cout << (time != INF ? time : -1) << endl;
-    cout << string(path.begin(), path.end()) << endl;
+    string path = directions[dest_x][dest_y];
+    cout << (time != INT32_MAX ? time : -1) << endl;
+    cout << path << endl;
+
     return 0;
 }
